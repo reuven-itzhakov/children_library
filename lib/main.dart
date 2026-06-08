e28469b0b6268ba52ca3d34e797df36661ad043e
reuven.itzhakov <itzhakovreuven@gmail.com>
2026-06-08 11:19:19 +0300

    Remove redundant import statements in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:children_library/DownloadPage.dart';
 import 'package:flutter/material.dart';
-
-
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
